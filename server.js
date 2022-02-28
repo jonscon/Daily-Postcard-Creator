ad512334f9a3ce35f87aaed2105d3394b69d2a59
Jonathan Cheng <jonathancheng777@gmail.com>
2022-02-27 16:06:31 -0800

    Update server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use("/images", express.static('images'));
 
 // Next, if no path is given, assume we will look at the postcard creation page
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/public/bow-acute-brother-creator.html');
+  response.sendFile(__dirname + '/public/index.html');
 });
 
 // Next, handle post request to upload an image
